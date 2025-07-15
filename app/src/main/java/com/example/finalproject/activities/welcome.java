cdf1e0ab129dde3d3281ab7af66f44064c79b2eb
ayatramy <ayatramy739@gmai.com>
2025-07-15 13:14:47 +0300

    ayatramy ayatra,y739@gmail.com

--- app/src/main/java/com/example/finalproject/activities/welcome.java
+++ app/src/main/java/com/example/finalproject/activities/welcome.java
@@ -1,36 +1,3 @@
-//package com.example.finalproject.activities;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.os.Handler;
-//import android.os.Looper;
-//import android.widget.ImageView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.example.finalproject.R;
-//
-//public class welcome extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_welcome);
-//
-//        ImageView welcome = findViewById(R.id.IV_quran);
-//
-//        // تأخير الانتقال لمدة 3 ثوانٍ باستخدام Handler
-//        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-//            Intent intent = new Intent(welcome.this, MainActivity.class);
-//            startActivity(intent);
-//            finish();
-//        }, 3000); // 3000 = 3 ثوانٍ
-//    }
-//}
-
-
-
-
 package com.example.finalproject.activities;
 
 import android.content.Intent;
